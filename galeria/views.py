bc62ba99a349eaa1580d2b2955fe7c047a39528a
patrickpbarroso <patrick.plbarroso@gmail.com>
2023-11-02 15:59:20 -0300

    Pegando os dados do bando de dados - index

--- galeria/views.py
+++ galeria/views.py
@@ -1,20 +1,9 @@
 from django.shortcuts import render
-
-dados = {
-    1: {'nome': 'Nebulosa de Carina',
-        'legenda': 'webbtelescope.org / NASA / James Webb'},
-    2: {'nome': 'Galáxia NGC 1079',
-        'legenda': 'nasa.or / NASA / Hublle'}
-}
+from galeria.models import Fotografia
 
 def index(request):
-    dados = {
-    1: {'nome': 'Nebulosa de Carina',
-        'legenda': 'webbtelescope.org / NASA / James Webb'},
-    2: {'nome': 'Galáxia NGC 1079',
-        'legenda': 'nasa.or / NASA / Hublle'}
-    }
-    return render(request, 'galeria/index.html', {'cards': dados})
+    fotografias = Fotografia.objects.all()
+    return render(request, 'galeria/index.html', {'cards': fotografias})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
