81fe1ac4679a0c9c588b8c9d25d8100bab885283
patrickpbarroso <patrick.plbarroso@gmail.com>
2023-11-09 19:32:39 -0300

    feature de busca

--- galeria/views.py
+++ galeria/views.py
@@ -2,10 +2,21 @@ from django.shortcuts import render, get_object_or_404
 from galeria.models import Fotografia
 
 def index(request):
-    # Busca apenas fotografias publicadas e ordena pela data de publicação
+    # View que retorna a página inicial da galeria
     fotografias = Fotografia.objects.order_by('-data_fotografia').filter(publicada=True)
     return render(request, 'galeria/index.html', {'cards': fotografias})
 
 def imagem(request, foto_id):
+    # View para página de imagem da galeria
     fotografia = get_object_or_404(Fotografia, pk=foto_id)
     return render(request, 'galeria/imagem.html', {'fotografia': fotografia})
+
+def buscar(request):
+    # View para pesquisar imagens na galeria
+    fotografias = Fotografia.objects.order_by('-data_fotografia').filter(publicada=True)
+
+    if 'buscar' in request.GET:
+        nome_a_buscar = request.GET['buscar']
+        if nome_a_buscar:
+            fotografias = fotografias.filter(nome__icontains=nome_a_buscar)
+    return render(request, 'galeria/index.html', {'cards': fotografias})
