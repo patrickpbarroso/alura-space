3e85daec36cddea0a7e544931f5d19f58616f190
patrickpbarroso <patrick.plbarroso@gmail.com>
2023-11-14 15:08:44 -0300

    Criando forms de login e cadastro

---
+++ usuarios/forms.py
@@ -0,0 +1,73 @@
+from django import forms
+
+class LoginForms(forms.Form):
+    # Formulário com dados de login de usuário
+    nome_login=forms.CharField(
+        label="Nome de usuário",
+        required=True,
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Ex.: Ana Santos"
+            }
+        )
+    )
+    senha=forms.CharField(
+        label="Senha",
+        required=True,
+        max_length=100,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Digite sua senha"
+            }
+        )
+    )
+
+class CadastroForms(forms.Form):
+    # Formulário com dados de cadastro de usuário
+    nome_cadastro=forms.CharField(
+        label="Nome de cadastro",
+        required=True,
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                "class":"form-control",
+                "placeholder":"Ex.: Ana Santos"
+            }
+        )
+    )
+    email=forms.EmailField(
+        label="Email",
+        required=True,
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                "class":"form-control",
+                "placeholder":"Ex.: ana.santos@email.com"
+            }
+        )
+    )
+    senha=forms.CharField(
+        label="Senha",
+        required=True,
+        max_length=100,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Digite sua senha"
+            }
+        )
+    )
+    senha2=forms.CharField(
+        label="Confirmação de Senha",
+        required=True,
+        max_length=100,
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Digite sua senha novamente"
+            }
+        )
+    )
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from usuarios.forms import LoginForms, CadastroForms
 
 def login(request):
     # View para realizar login de usuário existente na aplicação
-    return render(request, 'usuarios/login.html')
+    form = LoginForms()
+    return render(request, 'usuarios/login.html', {"form": form})
 
 def cadastro(request):
     # View para realizar cadastro de novo usuário na aplicação
-    return render(request, 'usuarios/cadastro.html')
+    form = CadastroForms()
+    return render(request, 'usuarios/cadastro.html', {"form": form})
