77082797af53470052148a99bc228ec3f1e83d88
patrickpbarroso <patrick.plbarroso@gmail.com>
2023-11-15 14:33:16 -0300

    Funcionalidade de logout e mensagens de alerta

--- galeria/views.py
+++ galeria/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from galeria.models import Fotografia
+from django.contrib import messages
 
 def index(request):
     # View que retorna a página inicial da galeria
+    if not request.user.is_authenticated:
+        messages.error(request, 'Faça login para acessar a galeria.')
+        return redirect('login')
+    
     fotografias = Fotografia.objects.order_by('-data_fotografia').filter(publicada=True)
     return render(request, 'galeria/index.html', {'cards': fotografias})
 
@@ -13,6 +18,10 @@ def imagem(request, foto_id):
 
 def buscar(request):
     # View para pesquisar imagens na galeria
+    if not request.user.is_authenticated:
+        messages.error(request, 'Faça login para realizar pesquisas.')
+        return redirect('login')
+    
     fotografias = Fotografia.objects.order_by('-data_fotografia').filter(publicada=True)
 
     if 'buscar' in request.GET:
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from usuarios.forms import LoginForms, CadastroForms
 from django.contrib.auth.models import User
-from django.contrib import auth
+from django.contrib import auth, messages
+
 def login(request):
     # View para realizar login de usuário existente na aplicação
     form = LoginForms()
@@ -20,8 +21,10 @@ def login(request):
         )
         if usuario is not None:
             auth.login(request, usuario)
+            messages.success(request, f'{nome} logado com sucesso')
             return redirect('index')
         else:
+            messages.error(request, 'Não foi possível realizar login, tente novamente')
             return redirect('login')
 
     return render(request, 'usuarios/login.html', {"form": form})
@@ -35,6 +38,7 @@ def cadastro(request):
 
         if form.is_valid():
             if form["senha_1"].value() != form["senha_2"].value():
+                messages.error(request, 'Senhas não são iguais. Tente novamente')
                 return redirect('cadastro')
         
         nome = form["nome_cadastro"].value()
@@ -42,6 +46,7 @@ def cadastro(request):
         senha = form["senha_1"].value()
 
         if User.objects.filter(username=nome).exists():
+            messages.error(request, 'Usuário já existe.')
             return redirect('cadastro')
 
         usuario = User.objects.create_user(
@@ -50,7 +55,12 @@ def cadastro(request):
             password=senha
         )
         usuario.save()
+        messages.success(request, 'Cadastro efetuado com sucesso')
         return redirect('login')
     
     return render(request, 'usuarios/cadastro.html', {"form": form})
 
+def logout(request):
+    auth.logout(request)
+    messages.success(request, 'Logout realizado com sucesso')
+    return redirect('login')
