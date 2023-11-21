569d7621faa253fb40834769132b345432359740
patrickpbarroso <patrick.plbarroso@gmail.com>
2023-11-21 20:53:18 -0300

    Edição de imagem

--- apps/galeria/views.py
+++ apps/galeria/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from apps.galeria.models import Fotografia
+from apps.galeria.forms import FotografiaForms
 from django.contrib import messages
 
 def index(request):
@@ -29,3 +30,36 @@ def buscar(request):
         if nome_a_buscar:
             fotografias = fotografias.filter(nome__icontains=nome_a_buscar)
     return render(request, 'galeria/index.html', {'cards': fotografias})
+
+def nova_imagem(request):
+    # View para adicionar nova imagem a galeria
+    if not request.user.is_authenticated:
+        messages.error(request, 'Faça login')
+        return redirect('login')
+    
+    form = FotografiaForms
+    if request.method == 'POST':
+        form = FotografiaForms(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Nova fotografia cadastrada!')
+            return redirect('index')
+    
+    return render(request, 'galeria/nova_imagem.html', {'form':form})
+
+def editar_imagem(request, foto_id):
+    # view para editar imagem
+    fotografia = Fotografia.objects.get(id=foto_id)
+    form = FotografiaForms(instance=fotografia)
+
+    if request.method == 'POST':
+        form = FotografiaForms(request.POST,request.FILES, instance=fotografia)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Fotografia editada com sucesso.')
+            return redirect('index')
+
+    return render(request, 'galeria/editar_imagem.html', {'form':form,'foto_id':foto_id})
+
+def deletar_imagem(request):
+    pass
